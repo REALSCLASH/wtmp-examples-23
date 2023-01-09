78661706552a2e2f7ae3515ed2537db6ef616a77
sclash <samuelpellikka@gmail.com>
2023-01-09 14:16:07 +0200

    initial commit for examples

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-console.log('Hello console!');
+//Author Realschs
+console.log('Eka webpack app!');
--- webpack.common.js
+++ webpack.common.js
@@ -19,7 +19,7 @@ module.exports = {
       },
     ]}),
     new HtmlWebpackPlugin({
-      title: 'WTMP Starter',
+      title: 'WTMP Student stuff',
       meta: {
         viewport: 'width=device-width, initial-scale=1.0'
       },
