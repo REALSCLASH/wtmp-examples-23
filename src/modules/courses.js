b86c9be19e631004c2de22505aeb9ecca4fc512d
sclash <samuelpellikka@gmail.com>
2023-01-29 21:37:02 +0200

    task3 done

--- src/modules/courses.js
+++
@@ -1,7 +0,0 @@
-export let Courses = [];
-
-export const initCourses = async () => {
-  const response = await fetch("/assets/Menu.json");
-  const data = await response.json();
-  Courses = Object.values(data.courses);
-};
--- src/modules/random-module.js
+++
@@ -1,8 +0,0 @@
-
-
-let Random = [];
-
-export const pickRandom = () => {
-  Random = [courses[Math.floor(Math.random() * courses.length)]];
-  refreshCourses(Random);
-};
